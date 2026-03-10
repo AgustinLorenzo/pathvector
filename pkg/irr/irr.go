f4a2d76f2dfe802fe3d37e9f2a94fbd2d1b1bcd4
Nate Sales <nate@natesales.net>
2026-03-10 02:26:00 +0100

    pathvector: remove IRR source filter arg from bgpq4
    Signed-off-by: Agustin Lorenzo <agustin.lorenzo@thinco.es>

--- pkg/irr/irr.go
+++ pkg/irr/irr.go
@@ -13,25 +13,13 @@ import (
 	"github.com/AgustinLorenzo/pathvector/pkg/config"
 )
 
-// withSourceFilter returns the AS set or AS set with the IRR source replaced with the -S SOURCE syntax
-// AS34553 -> AS34553
-// RIPE::AS34553 -> -S RIPE AS34553
-func withSourceFilter(asSet string) string {
-	if strings.Contains(asSet, "::") {
-		log.Debugf("Using IRRDB source from AS set %s", asSet)
-		tokens := strings.Split(asSet, "::")
-		return fmt.Sprintf("-S %s %s", tokens[0], tokens[1])
-	}
-	return asSet
-}
-
 // PrefixSet uses bgpq4 to generate a prefix filter and return only the filter lines
 func PrefixSet(macro string, family uint8, irrServer string, queryTimeout uint, bgpqArgs string) ([]string, error) {
 	var prefixes []string
 
 	for _, asSet := range strings.Split(macro, " ") {
 		// Run bgpq4 for BIRD format with aggregation enabled
-		cmdArgs := fmt.Sprintf("-h %s -Ab%d %s", irrServer, family, withSourceFilter(asSet))
+		cmdArgs := fmt.Sprintf("-h %s -Ab%d %s", irrServer, family, asSet)
 		if bgpqArgs != "" {
 			cmdArgs = bgpqArgs + " " + cmdArgs
 		}
@@ -63,7 +51,7 @@ func PrefixSet(macro string, family uint8, irrServer string, queryTimeout uint, 
 // ASMembers uses bgpq4 to generate an AS member set
 func ASMembers(asSet string, irrServer string, queryTimeout uint, bgpqArgs string) ([]uint32, error) {
 	// Run bgpq4 for BIRD format with aggregation enabled
-	cmdArgs := fmt.Sprintf("-h %s -tj %s", irrServer, withSourceFilter(asSet))
+	cmdArgs := fmt.Sprintf("-h %s -tj %s", irrServer, asSet)
 	if bgpqArgs != "" {
 		cmdArgs = bgpqArgs + " " + cmdArgs
 	}
